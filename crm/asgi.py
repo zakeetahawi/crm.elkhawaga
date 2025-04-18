2c694ea20161c5936b382117e3864cfe6b31c584
zakeetahawi <zakee.tahawi@elkhawaga.com>
2025-04-18 18:44:25 +0200

    Add ASGI support and update serverless configuration

--- crm/asgi.py
+++ crm/asgi.py
@@ -1,16 +1,14 @@
-"""
-ASGI config for crm project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from mangum import Mangum
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm.settings')
 
-application = get_asgi_application()
+# Get the ASGI application
+django_asgi_app = get_asgi_application()
+
+# Create Mangum handler
+handler = Mangum(django_asgi_app, lifespan="off")
+
+# Export handler for AWS Lambda / Netlify Functions
+application = handler
--- netlify/functions/django.py
+++ netlify/functions/django.py
@@ -6,23 +6,26 @@ import json
 root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, root_path)
 
-# Import the Mangum handler from our WSGI file
-from crm.wsgi import handler as wsgi_handler
+# Import the Mangum handler from our ASGI file
+from crm.asgi import handler as asgi_handler
 
 def handler(event, context):
     try:
-        # Handle the request using Mangum
-        response = wsgi_handler(event, context)
+        # Initialize response from ASGI handler
+        response = asgi_handler(event, context)
         
         # Ensure headers exist
         if 'headers' not in response:
             response['headers'] = {}
-        
-        # Add CORS headers
+            
+        # Add security headers
         response['headers'].update({
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'X-Content-Type-Options': 'nosniff',
+            'X-Frame-Options': 'DENY',
+            'X-XSS-Protection': '1; mode=block'
         })
         
         return response
