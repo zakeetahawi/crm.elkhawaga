a0b2f010c50bdc90c6af7d4abb3823f525bab29c
zakeetahawi <zakee.tahawi@elkhawaga.com>
2025-04-18 14:30:21 +0200

    تحديث إعدادات النشر لاستخدام pyenv وPython 3.7.13

--- build.sh
+++ build.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Python version:"
-python --version
-
-echo "Installing dependencies..."
-pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Setting up directories..."
 mkdir -p staticfiles media
 
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-echo "Applying migrations..."
 python manage.py migrate --noinput
 
 echo "Build completed!"
